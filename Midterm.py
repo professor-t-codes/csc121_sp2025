fe61ddadb7b4d69493c34997c0dd05ad35b59ea4
Richard T <74796181+rtillies@users.noreply.github.com>
2024-12-29 15:57:20 -0500

    Midterm code

---
+++ Midterm.py
@@ -0,0 +1,20 @@
+number = list(range(4, 16, 3))
+print(number)
+
+word = 'house'
+# word[2] = 'X'
+print(word)
+
+text = "WakeTechCommunityCollege"
+print(text[:5])
+
+speed = 56
+if speed in range(0, 56):
+  print("In limit")
+else:
+  print("Out of limit")
+
+  num_list = []
+  for num in range(0, 35, 5):
+      num_list.append(num)
+  print(num_list)
